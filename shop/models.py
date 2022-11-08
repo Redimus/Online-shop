a4eb773fe28d9a88b030f16352f0aacb860f5d8c
Redimus <Redimus@mail.ru>
2022-11-08 23:27:01 +0300

    Edit models

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Category(models.Model):
     """ Категории товаров """
-    name = models.CharField(max_length=50)
+    name = models.CharField('Название', max_length=50)
+    description = models.TextField('Описание', null=True)
+    slug = models.SlugField(max_length=160, null=True)
 
     class Meta:
         ordering = ('name',)
@@ -16,15 +18,15 @@ class Category(models.Model):
 
 class Product(models.Model):
     """ Товары """
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    price = models.SmallIntegerField()
-    sale = models.SmallIntegerField(blank=True)
-    photo_preview = models.ImageField(upload_to='product_photos/')
-    photo_overview = models.ImageField(upload_to='product_photos/')
-    is_published = models.BooleanField(default=False)
-    available = models.BooleanField(default=False)
+    title = models.CharField('Название', max_length=50)
+    description = models.TextField('Описание', null=True)
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    price = models.SmallIntegerField('Цена')
+    sale = models.SmallIntegerField('Скидка (новая цена)', null=True, blank=True)
+    photo_preview = models.ImageField('Основное фото', upload_to='product_photos/')
+    photo_overview = models.ImageField('Дополнительные фото', upload_to='product_photos/')
+    is_published = models.BooleanField('Опубликовать', default=False)
+    available = models.BooleanField('В наличии', default=False)
 
     class Meta:
         ordering = ('title',)
