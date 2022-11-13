91190be835093258c7c17b558e874a114d7bd924
Redimus <Redimus@mail.ru>
2022-11-13 22:59:09 +0300

    Create blog app, create html templates, edit links on main page.

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='main_page'),
+    path('shop/', shop, name='shop'),
+    path('delivery/', delivery, name='delivery'),
+    path('about_us/', about_us, name='about_us'),
+    path('contacts/', contacts, name='contacts'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def shop(request):
+    return render(request, 'shop/shop.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+
+def delivery(request):
+    return render(request, 'delivery.html')
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
