4717fa20c537c1dbbffb560aba2096903f9b344d
Redimus <Redimus@mail.ru>
2022-11-26 13:24:08 +0300

    Edit main page, add carousel.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Посты для блога"""
+    title = models.CharField('Заголовок', max_length=150)
+    description = models.TextField('Текст записи')
+    photo = models.ImageField('Фото', upload_to='blog_photo', blank=True)
+    publication_date = models.DateField('Дата публикации')
+    is_published = models.BooleanField('Опубликовать', default=False)
+
